d9ff94ceee27710be48474ded044c9b074981dd9
Keith <keithwilfert@gmail.com>
2025-09-10 14:29:36 -0400

    new commitgit add .

--- main.java
+++ main.java
@@ -3,6 +3,8 @@
 
  public class main {
         public static void main(String[] args) {
+            int i = 0;
+            System.out.println(i);
             System.out.println("Hello, World!");
         }
     }
