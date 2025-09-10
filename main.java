1be2ccdc93e5a1b87e2604b6e09fdef3418c94d3
Keith <keithwilfert@gmail.com>
2025-09-10 14:33:38 -0400

    reset commit

--- main.java
+++ main.java
@@ -1,10 +1,4 @@
 //hello im adding changes to the file
 
 
- public class main {
-        public static void main(String[] args) {
-            int i = 0;
-            System.out.println(i);
-            System.out.println("Hello, World!");
-        }
-    }
+ 
